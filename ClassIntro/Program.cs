0c540715ec6a387634da3db2a58a96c8f0454142
Metehan Gök <Mthngk@hotmail.com>
2021-01-30 23:25:50 +0300

    20 ocak ders sonu

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Engin";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.İzlenmeOranı = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.İzlenmeOranı = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Berkay Bilgin";
+            kurs3.İzlenmeOranı = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Metehan GÖK";
+            kurs4.İzlenmeOranı = 54;
+
+            //Console.WriteLine(kurs2.KursAdi + ": " + kurs2.Egitmen);
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2,kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi+ ":"+ kurs.Egitmen);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int İzlenmeOranı { get; set; }
+    }
+}
